1628e646da1bfcdf2ff4200b5fd4d4defbabadba
hrrom <freerom@gmail.com>
2016-03-25 06:56:36 +0200

    Initial commit

---
+++ app/src/main/java/com/ukr/mfyp/ImageAdapter.java
@@ -0,0 +1,73 @@
+package com.ukr.mfyp;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private String[] images;
+    private Context context;
+
+    public ImageAdapter(Context context, String[] images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @Override
+    public ImageAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item, viewGroup, false);
+
+        // Changing by half width of items of RecycleView
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        int width = (Resources.getSystem().getDisplayMetrics().widthPixels) / 2 - 24;
+        LinearLayout.LayoutParams imgParams = new LinearLayout.LayoutParams(width, LinearLayout.LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(imgParams);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+       Picasso
+               .with(context)
+               .load(images[i])
+               .fit()
+               .centerInside()
+               .placeholder(R.mipmap.clock)
+               .error(R.mipmap.no_image)
+               .into(viewHolder.img_android);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ImageView img_android;
+
+        public ViewHolder(View v) {
+            super(v);
+            img_android = (ImageView)v.findViewById(R.id.item_view);
+            img_android.setOnClickListener(this);
+
+        }
+        /** Process pressing on item of RecycleView and shows Toast message   */
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            Toast.makeText(context, "Image #" + position + " is clicked", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/ukr/mfyp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.ukr.mfyp;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    /** Handler for Back Arrow in ActionBar */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    /** initialization for Back Arrow, RecycleView */
+    private void initViews(){
+        // display Back Arrow in ActionBar
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        Resources res = getResources();
+        String[] images = res.getStringArray(R.array.images_array);
+        ImageAdapter adapter = new ImageAdapter(getApplicationContext(),images);
+        recyclerView.setAdapter(adapter);
+    }
+
+    /** Process pressing on View and shows Toast message         */
+    public void onClickViewHandler(View v){
+        int id = v.getId();                                 // get  ID of view
+        String IDString = "No ID";
+        if(id != View.NO_ID) {                              // check ID isn't empty
+            Resources res = v.getResources();               // get resources
+            if(res != null)
+                IDString = res.getResourceEntryName(id);    // get ID string entry
+        }
+        Toast.makeText(this, IDString, Toast.LENGTH_SHORT).show();
+    }
+}
